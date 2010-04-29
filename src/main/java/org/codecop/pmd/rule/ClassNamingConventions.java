593c1263d5b710fcff4fef7cee592e25d0109677
Peter Kofler <peter.kofler@code-cop.org>
2010-04-29 22:09:27 +0200

    added parameters to ClassNaming andSignatureThrows rules, added test cases

--- src/main/java/org/codecop/pmd/rule/ClassNamingConventions.java
+++ src/main/java/org/codecop/pmd/rule/ClassNamingConventions.java
@@ -1,30 +1,47 @@
 package org.codecop.pmd.rule;
 
+import java.util.Map;
+
 import net.sourceforge.pmd.AbstractJavaRule;
+import net.sourceforge.pmd.PropertyDescriptor;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.properties.IntegerProperty;
 
 /**
  * Changed Class name rule to allow underscore before _Stub and _Core (RMI). Added some additional tests.
  * @author PMD 3.1 - Copied from PMD 3.1 Source Edition.
  * @author PMD 3.7 - updated
+ * @author Peter Kofler
  */
 public class ClassNamingConventions extends AbstractJavaRule {
 
+   private int upperCaseLen;
+
+   private static final PropertyDescriptor upperCaseLenDescriptor = new IntegerProperty("upperCaseLen", "Allowed length of upper case only names", 3, 1.0f);
+   private static final Map<String, PropertyDescriptor> propertyDescriptorsByName = asFixedMap(new PropertyDescriptor[] { upperCaseLenDescriptor });
+
+   @Override
+   protected Map<String, PropertyDescriptor> propertiesByName() {
+      return propertyDescriptorsByName;
+   }
+
    @Override
    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+      upperCaseLen = getIntProperty(upperCaseLenDescriptor);
+
       final String className = node.getImage();
 
       // first letter must be upper case letter
       if (Character.isLowerCase(className.charAt(0)) || !Character.isLetter(className.charAt(0))) {
-         // PMD 3.1
+         // --- PMD 3.1
          // RuleContext ctx = (RuleContext) data;
          // ctx.getReport().addRuleViolation(createRuleViolation(ctx, node, getMessage()));
-         // PMD 3.7
+         // --- PMD 3.7
          addViolation(data, node);
       }
 
       // must not be uppercase only
-      if (className.toUpperCase().equals(className) && className.length() > 6) {
+      if (className.toUpperCase().equals(className) && className.length() > upperCaseLen) {
          addViolation(data, node);
       }
 
@@ -34,7 +51,7 @@ public class ClassNamingConventions extends AbstractJavaRule {
       }
 
       // must not contain underscore but RMI stubs and Core pattern
-      else if (className.indexOf("_") >= 0 && !className.endsWith("_Stub") && !className.endsWith("_Core")) {
+      else if (className.indexOf('_') >= 0 && !className.endsWith("_Stub") && !className.endsWith("_Core")) {
          addViolation(data, node);
       }
 
--- src/main/java/org/codecop/pmd/rule/ExceptionSignatureDeclaration.java
+++ src/main/java/org/codecop/pmd/rule/ExceptionSignatureDeclaration.java
@@ -2,35 +2,50 @@ package org.codecop.pmd.rule;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import net.sourceforge.pmd.AbstractJavaRule;
+import net.sourceforge.pmd.PropertyDescriptor;
 import net.sourceforge.pmd.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.ast.ASTConstructorDeclaration;
 import net.sourceforge.pmd.ast.ASTImportDeclaration;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.ast.ASTName;
 import net.sourceforge.pmd.ast.Node;
+import net.sourceforge.pmd.properties.BooleanProperty;
 
 /**
  * Changed Exception Signature rule to allow also Spring mock.
  * @author PMD 3.1 - Copied from PMD 3.1 Source Edition.
  * @author PMD 3.7 - updated
+ * @author Peter Kofler
  */
 public class ExceptionSignatureDeclaration extends AbstractJavaRule {
 
    private static final List<String> allowedMethods = Arrays.asList(new String[] { "setUp", "tearDown", "onSetUp", "onTearDown", });
 
+   private boolean ignoreTests;
    private boolean junitImported;
+
+   private static final PropertyDescriptor ignoreTestsDescriptor = new BooleanProperty("ignoreTests", "Ignore test methods", false, 1.0f);
+
+   private static final Map<String, PropertyDescriptor> propertyDescriptorsByName = asFixedMap(new PropertyDescriptor[] { ignoreTestsDescriptor });
+
+   @Override
+   protected Map<String, PropertyDescriptor> propertiesByName() {
+      return propertyDescriptorsByName;
+   }
 
    @Override
    public Object visit(ASTCompilationUnit node, Object o) {
       junitImported = false;
+      ignoreTests = getBooleanProperty(ignoreTestsDescriptor);
       return super.visit(node, o);
    }
 
    //   @Override
    //   public Object visit(ASTPackageDeclaration node, Object o) {
-   //      // allow any classes which reside in  a .junit. or .httpunit package too
+   //      // allow any classes which reside in  a .junit. or .httpunit package
    //      ASTName packageName = node.getFirstChildOfType(ASTName.class);
    //      if (packageName != null && (packageName.getImage().endsWith(".junit") || packageName.getImage().endsWith(".httpunit"))) {
    //         junitImported = true;
@@ -41,7 +56,10 @@ public class ExceptionSignatureDeclaration extends AbstractJavaRule {
    @Override
    public Object visit(ASTImportDeclaration node, Object o) {
       final String importName = node.getImportedName();
-      if (importName.indexOf("junit.") != -1 || importName.indexOf(".httpunit.") != -1 || importName.indexOf("org.springframework.test.") != -1) {
+      if (importName.startsWith("junit.") || // JUnit 3.x
+            importName.startsWith("org.junit.") || // JUnit 4.x
+            importName.indexOf(".httpunit.") != -1 || // 
+            importName.startsWith("org.springframework.test.")) { // Spring Mock
          junitImported = true;
       }
       return super.visit(node, o);
@@ -50,7 +68,10 @@ public class ExceptionSignatureDeclaration extends AbstractJavaRule {
    @Override
    public Object visit(ASTMethodDeclaration methodDeclaration, Object o) {
       final String methodName = methodDeclaration.getMethodName();
-      if ((allowedMethods.contains(methodName) || methodName.startsWith("test")) && junitImported) {
+      if (allowedMethods.contains(methodName) && junitImported) {
+         return super.visit(methodDeclaration, o);
+      }
+      if (ignoreTests && methodName.startsWith("test")) {
          return super.visit(methodDeclaration, o);
       }
 
@@ -79,9 +100,9 @@ public class ExceptionSignatureDeclaration extends AbstractJavaRule {
    private void evaluateExceptions(List<ASTName> exceptionList, Object context) {
       for (ASTName exception : exceptionList) {
          if (hasDeclaredExceptionInSignature(exception)) {
-            // PMD 3.1
+            // --- PMD 3.1
             // context.getReport().addRuleViolation(createRuleViolation(context, exception));
-            // PMD 3.7
+            // --- PMD 3.7
             addViolation(context, exception);
          }
       }
