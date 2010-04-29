529808d6088d634c30be540d45daefa4112c5159
Peter Kofler <peter.kofler@code-cop.org>
2010-04-29 23:33:13 +0200

    add testcases, format and fix old code cop rules

--- src/test/java/org/codecop/pmd/rule/CodeCopRulesTest.java
+++ src/test/java/org/codecop/pmd/rule/CodeCopRulesTest.java
@@ -6,10 +6,20 @@ import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 
 public class CodeCopRulesTest extends SimpleAggregatorTst {
 
+   private static final String RULESET = "codecop";
+
    @Before
    public void setUp() {
-      addRule("codecop", "ClassNamingConventions");
-      addRule("codecop", "SignatureDeclareThrowsException");
+      addRule(RULESET, "ClassNamingConventions");
+      addRule(RULESET, "SignatureDeclareThrowsException");
+      addRule(RULESET, "JumbledIterator");
+      addRule(RULESET, "UnintendedEnvUsage");
+      addRule(RULESET, "JunitSetupDoesNotCallSuper");
+      addRule(RULESET, "ParameterNameWithP");
+      addRule(RULESET, "InterfaceNamesEndWithIF");
+      addRule(RULESET, "NonFinalFieldInException");
+      addRule(RULESET, "AvoidPrivateGetterAndSetter");
+      addRule(RULESET, "MembersMustBePrivate");
    }
 
    public static junit.framework.Test suite() {
