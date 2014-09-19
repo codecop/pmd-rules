e6bd035bbbfc0a3f61dc9fa36232950eb79f2677
Peter Kofler <peter.kofler@code-cop.org>
2014-09-20 00:29:49 +0200

    add empty constraints rule file

---
+++ src/test/java/org/codecop/pmd/constraints/ConstraintsRulesTest.java
@@ -0,0 +1,24 @@
+package org.codecop.pmd.constraints;
+
+import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+import org.junit.Before;
+
+/**
+ * Test the rules in the <code>constraints</code> ruleset.
+ * @author <a href="http://www.code-cop.org/">Peter Kofler</a>
+ */
+public class ConstraintsRulesTest extends SimpleAggregatorTst {
+
+    private static final String RULESET = "java-constraints";
+
+    @Before
+    public void setUp() {
+        // addRule(RULESET, "ClassNamingConventions");
+    }
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(ConstraintsRulesTest.class);
+    }
+
+}
