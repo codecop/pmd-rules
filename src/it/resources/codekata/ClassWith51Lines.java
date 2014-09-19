f498f08fbf70f57d7985e8ce12b6359cd61ad3a2
Peter Kofler <peter.kofler@code-cop.org>
2014-09-20 01:21:12 +0200

    ignore fake methods in test data

--- src/it/resources/codekata/ClassWith51Lines.java
+++ src/it/resources/codekata/ClassWith51Lines.java
@@ -2,70 +2,70 @@ package codekata;
 
 public class ClassWith51Lines {
 
-    void okShortMethod1() {
+    void okShortMethod1() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod2() {
+    void okShortMethod2() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod3() {
+    void okShortMethod3() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod4() {
+    void okShortMethod4() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod5() {
+    void okShortMethod5() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod6() {
+    void okShortMethod6() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod7() {
+    void okShortMethod7() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod8() {
+    void okShortMethod8() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod9() {
+    void okShortMethod9() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
     }
 
-    void okShortMethod10() {
+    void okShortMethod10() { // NOPMD "void"
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
