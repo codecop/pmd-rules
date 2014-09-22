84591db4887c7ae59cb44b9f1a985bfc66d36296
Peter Kofler <peter.kofler@code-cop.org>
2014-09-22 22:56:19 +0200

    split code kata rules into rules by constraint

--- src/it/run_code-kata-rules.bat
+++ src/it/run_code-kata-rules.bat
@@ -2,6 +2,10 @@
 @call env_pmd.bat
 
 call %PMD_HOME%\bin\pmd.bat -d resources -R ../main/resources/rulesets/java/code-kata.xml
+echo ------------------------------------------------------------
+call %PMD_HOME%\bin\pmd.bat -d resources -R ../main/resources/rulesets/java/object-calisthenics.xml
+echo ------------------------------------------------------------
+call %PMD_HOME%\bin\pmd.bat -d resources -R ../main/resources/rulesets/java/brutal-refactoring.xml
 
 @endlocal  
 pause
