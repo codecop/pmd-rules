d8cc1981d0d598e72923e4746e765176474a2a88
Peter Kofler <peter.kofler@code-cop.org>
2014-12-07 00:57:50 +0100

    add pause and show site for run_all script before deployment

--- build_with_all_profiles.bat
+++ build_with_all_profiles.bat
@@ -1,3 +1,12 @@
+@call profile160
+@pause
+@echo running with PMD 5.0
 call mvn clean verify site
+@start target\site\index.html
+@pause
+@echo running with PMD 5.1
 call mvn clean verify -Dpmd=5.1
+@pause
+@echo running with PMD 5.2
 call mvn clean verify -Dpmd=5.x
+@pause
