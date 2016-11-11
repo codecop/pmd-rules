62740577b15e85d38c3ee9b0c4756ccf34233940
Peter Kofler <peter.kofler@code-cop.org>
2016-11-11 11:58:13 +0100

    Support PMD up to latest version in plugins. No code change needed. Thank you PMD team.

--- build_with_all_profiles.bat
+++ build_with_all_profiles.bat
@@ -1,12 +1,34 @@
+@setlocal
 @call profile160
 @pause
+
 @echo running with PMD 5.0
 call mvn clean verify site
 @start target\site\index.html
 @pause
+
 @echo running with PMD 5.1
 call mvn clean verify -Dpmd=5.1
 @pause
+
 @echo running with PMD 5.2
+call mvn clean verify -Dpmd=5.2
+@pause
+
+@echo running with PMD 5.3
+call mvn clean verify -Dpmd=5.3
+@pause
+@endlocal
+
+@setlocal
+@call profile170
+@pause
+
+@echo running with PMD 5.4
+call mvn clean verify -Dpmd=5.4
+@pause
+
+@echo running with PMD 5.5
 call mvn clean verify -Dpmd=5.x
 @pause
+@endlocal
