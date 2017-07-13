4053b0edae5c75ddb724fb0b5f2a6ca7c4611816
Peter Kofler <peter.kofler@code-cop.org>
2017-07-13 10:17:16 +0200

    PMD 5.3, 5.4 and 5.5 all have same code compatibility.

--- build_with_all_profiles.bat
+++ build_with_all_profiles.bat
@@ -8,16 +8,19 @@ rem mvn site
 rem @start target\site\index.html
 @pause
 
+@rem 5.1.3 ... 2014
 @echo running with PMD 5.1
-call mvn2 clean verify -Dpmd=5.1
+call mvn2 clean test -Dpmd=5.0
 @pause
 
+@rem 5.2.3 ... 2014
 @echo running with PMD 5.2
-call mvn2 clean verify -Dpmd=5.2
+call mvn2 clean verify -Dpmd=5.2 -Dpmd.version=5.2.3
 @pause
 
+@rem 5.3.8 ... 2016
 @echo running with PMD 5.3
-call mvn2 clean verify -Dpmd=5.3
+call mvn2 clean test -Dpmd=5.2 -Dpmd.version=5.3.8
 @pause
 @endlocal
 
@@ -25,11 +28,14 @@ call mvn2 clean verify -Dpmd=5.3
 @call profile170
 @pause
 
+@rem 5.4.3 ... 2016
 @echo running with PMD 5.4
-call mvn clean verify -Dpmd=5.4
+call mvn clean verify -Dpmd=5.2 -Dpmd.version=5.4.6
 @pause
 
+@rem 5.5.7 ... 2017
 @echo running with PMD 5.5
-call mvn clean verify -Dpmd=5.x
+call mvn clean test -Dpmd=5.2 -Dpmd.version=5.5.7
 @pause
+
 @endlocal
