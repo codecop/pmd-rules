8328dc62fa758c7ce6b0c0b22903e4593a2dceda
Peter Kofler <peter.kofler@code-cop.org>
2018-02-07 20:17:43 +0100

    Use reflection to access Properties. These rules will run in PMD 6.

--- src/main/java/org/codecop/pmd/BooleanPropertyAdapter.java
+++ src/main/java/org/codecop/pmd/BooleanPropertyAdapter.java
@@ -1,7 +1,9 @@
 package org.codecop.pmd;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import net.sourceforge.pmd.Rule;
-import net.sourceforge.pmd.lang.rule.properties.BooleanProperty;
 
 /**
  * Wrapper around changed API used in rules. <br>
@@ -16,19 +18,71 @@ public class BooleanPropertyAdapter {
     private final Boolean defaultValue;
     private final float theUIOrder;
 
+    private Object property5;
+    private Object property6;
+    private UnsupportedOperationException failed;
+
     public BooleanPropertyAdapter(String theName, String theDescription, Boolean defaultValue, float theUIOrder) {
         this.theName = theName;
         this.theDescription = theDescription;
         this.defaultValue = defaultValue;
         this.theUIOrder = theUIOrder;
+
+        this.property5 = createPropertyOfType("net.sourceforge.pmd.lang.rule.properties.BooleanProperty"); // PMD 5.x
+        this.property6 = createPropertyOfType("net.sourceforge.pmd.properties.BooleanProperty"); // PMD 6.x
     }
 
-    private BooleanProperty asProperty() {
-        return new BooleanProperty(theName, theDescription, defaultValue, theUIOrder);
+    private Object createPropertyOfType(String propertyClassName) {
+        try {
+
+            // return new IntegerProperty(theName, theDescription, min, max, theDefault, theUIOrder);
+            Class<?> factory = Class.forName(propertyClassName);
+            @SuppressWarnings("rawtypes")
+            Class[] argumentTypes = new Class[] { String.class, String.class, Boolean.class, float.class };
+            Constructor<?> constructor = factory.getConstructor(argumentTypes);
+            Object[] arguments = new Object[] { theName, theDescription, defaultValue, theUIOrder };
+            return constructor.newInstance(arguments);
+
+        } catch (Exception reflectionFailed) {
+            String combinedMessage = "PMD API:\n" + reflectionFailed.getMessage();
+            this.failed = new UnsupportedOperationException(combinedMessage, reflectionFailed);
+            return null;
+        }
+    }
+
+    private Object getProperty() {
+        if (property5 != null) {
+            return property5;
+        }
+        if (property6 != null) {
+            return property6;
+        }
+        throw failed;
     }
 
     public static boolean getProperty(Rule rule, BooleanPropertyAdapter propertyAdapter) {
-        return rule.getProperty(propertyAdapter.asProperty());
+        Object property = propertyAdapter.getProperty();
+
+        try {
+
+            // return rule.getProperty(property);
+            return (Boolean) findGetter(rule).invoke(rule, new Object[] { property });
+
+        } catch (Exception reflectionFailed) {
+            String combinedMessage = "PMD API:\n" + reflectionFailed.getMessage();
+            UnsupportedOperationException failed = new UnsupportedOperationException(combinedMessage, reflectionFailed);
+            throw failed;
+        }
+    }
+
+    private static Method findGetter(Rule rule) throws NoSuchMethodException {
+        // return rule.getProperty(property);
+        for (Method method : rule.getClass().getMethods()) {
+            if (method.getName().equals("getProperty") && method.getParameterTypes().length == 1) {
+                return method;
+            }
+        }
+        throw new NoSuchMethodException("getProperty");
     }
 
 }
