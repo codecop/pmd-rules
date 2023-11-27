b8401eb58c8e41499869adbd86add50e509af902
Peter Kofler <peter.kofler@code-cop.org>
2023-11-27 21:02:23 +0100

    Run with a PMD version from each year.

--- build_with_all_profiles.bat
+++ build_with_all_profiles.bat
@@ -9,17 +9,9 @@ rem mvn site
 rem @start target\site\index.html
 @pause
 
-@echo running with PMD 6.6
-call mvn clean test -Dpmd.version=6.6.0
-@pause
-
 @rem 6.11.0 ... 2019
 @echo running with PMD 6.11
 call mvn clean test -Dpmd.version=6.11.0
-@pause
-
-@echo running with PMD 6.17
-call mvn clean test -Dpmd.version=6.17.0
 @pause
 
 @rem 6.21.0 ... 2020
@@ -27,7 +19,17 @@ call mvn clean test -Dpmd.version=6.17.0
 call mvn clean test -Dpmd.version=6.21.0
 @pause
 
-@rem 6.55.0 ... 2023
+@rem 6.31.0 ... 2021
+@echo running with PMD 6.31
+call mvn clean test -Dpmd.version=6.31.0
+@pause
+
+@rem 6.42.0 ... 2022
+@echo running with PMD 6.42
+call mvn clean test -Dpmd.version=6.42.0
+@pause
+
+@rem 6.55.0 ... last 2023
 @echo running with PMD 6.55
 call mvn clean test -Dpmd.version=6.55.0
 @pause
